cf09b029dc4f84dd61b118a7c24d1976902c1bb8
Naohiro Tsuji <naoiro2g@code2create.club>
2021-06-27 09:10:41 +0900

    initial commit

---
+++ demo.py
@@ -0,0 +1,6 @@
+import axis
+
+#axis.reset_Minecraft_World()
+#axis.draw_XYZ_axis()
+# axis.clear_XYZ_axis()
+axis.draw_XYZ_axis()
---
+++ param_MCPI.py
@@ -0,0 +1,20 @@
+# virtical levels
+Y_TOP = 63         # the top where blocks can be set
+Y_SEA = -1         # the sea level
+
+# axis
+AXIS_WIDTH = 20    # x, z: -20 .. 0 .. 20
+AXIS_TOP = 40
+AXIS_Y_V_ORG = 20  # y of virtual origin
+AXIS_BOTTOM = 0    # y: 0 .. 20 .. 40
+
+# connection port
+PORT_MC  = 4711
+
+# blobk ID
+AIR = 0
+STONE = 1  
+GRASS_BLOCK = 2
+GOLD_BLOCK = 41
+IRON_BLOCK = 42
+SEA_LANTERN_BLOCK = 169
